2e5f57c6d151a0b7607387d435af4a8f34e1d608
Mohammad Uddin <mtaslimu010@gmail.com>
2021-03-22 17:24:58 -0400

    Midterm

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,25 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String a = "MOM";
+
+        String reverse = "";
+        for (int i = a.length() -1; i >= 0; i--){
+            reverse +=a.charAt(i);
+        }
+
+        boolean palindrome = true;
+        for (int i = 0; i < a.length(); i++){
+            if (a.charAt(i) != reverse.charAt(i)){
+                palindrome = false;
+            }
+            if (palindrome){
+                System.out.println("String is Palindrome!");
+            }else {
+                System.out.println("String is not a Palindrome!");
+            }
+
+        }
+
     }
 }
--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -7,10 +7,25 @@ public class Permutation {
 
     public static void main(String[] args) {
 
-        /*
-         * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
+        /* Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
          * Write Java program to compute all Permutation of a String
-         *
          */
+        premute("ABC");
+    }
+    public static void  premute (String full){
+        if (full == null || full.length() == 0){
+            return;
+        }
+        premute("", full);
+    }
+    public static void premute(String prefix, String remaining){
+        if (remaining.length() == 0){
+            System.out.println(prefix);
+            return;
+        }
+        for (int i = 0; i < remaining.length(); i++){
+            premute(prefix + remaining.charAt(i), remaining.substring(0, i) +
+                    remaining.substring(i + 1, remaining.length()));
+        }
     }
 }
